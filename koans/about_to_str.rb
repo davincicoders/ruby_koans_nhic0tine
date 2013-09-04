7685a7abdc22d17d9353d018f817e2c0bd6014ce
Andy Nguyen <nhi.atn@gmail.com>
2013-09-03 20:25:34 -0600

    About to Str
    * to_str (string object 'behavior')  / 
      to_s (string representation of object)
    
    [#54145677]

--- koans/about_to_str.rb
+++ koans/about_to_str.rb
@@ -14,18 +14,13 @@ class AboutToStr < Neo::Koan
   end
 
   def test_normally_objects_cannot_be_used_where_strings_are_expected
-    assert_raise(___) do
+    assert_raise(TypeError) do
       File.exist?(CanNotBeTreatedAsString.new)
     end
   end
 
   # ------------------------------------------------------------------
 
-  class CanBeTreatedAsString
-    def initialize
-      raise :hell
-    end
-  
     def to_s
       "string-like"
     end
@@ -37,35 +32,22 @@ class AboutToStr < Neo::Koan
 
   def test_to_str_also_returns_a_string_representation
     like_a_string = CanBeTreatedAsString.new
-    assert_equal __, like_a_string.to_str
+    assert_equal "string-like", like_a_string.to_str
   end
 
   def test_to_str_allows_objects_to_be_treated_as_strings
-    assert_equal __, File.exist?(CanBeTreatedAsString.new)
+    assert_equal false, File.exist?(CanBeTreatedAsString.new)
   end
 
   # ------------------------------------------------------------------
 
   def acts_like_a_string?(string)
-    string = string.to_str if string.respond_to?(:to_str) && false
+    string = string.to_str if string.respond_to?(:to_str)
     string.is_a?(String)
   end
 
   def test_user_defined_code_can_check_for_to_str
-    assert_equal __, acts_like_a_string?(CanNotBeTreatedAsString.new)
-    assert_equal __,  acts_like_a_string?(CanBeTreatedAsString.new)
-  end
-  
-  def test_andy_completed_ruby_koans
-    3.times { puts "**********************************************" }
-    2.times { puts "********                              ********" }
-    1.times { puts "********   GOOD JOB ANDY!!!           ********" }
-    1.times { puts "********                              ********" }
-    1.times { puts "********   Don't forget to run rake   ********" }
-    1.times { puts "********     to see the whole suite   ********" }
-    1.times { puts "********       pass!!!                ********" }
-    2.times { puts "********                              ********" }
-    3.times { puts "**********************************************" }
-    assert false
+    assert_equal false, acts_like_a_string?(CanNotBeTreatedAsString.new)
+    assert_equal true,  acts_like_a_string?(CanBeTreatedAsString.new)
   end
 end
